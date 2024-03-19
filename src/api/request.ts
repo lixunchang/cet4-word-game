a58ca35f2ef2176621ce844f50975edabc63dd13
gexinpai <783263503@qq.com>
2024-03-19 10:39:10 +0800

    feat: 构建项目

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,110 +1,112 @@
-import axios from 'axios';
-import { ElMessage } from 'element-plus'  // 引入el 提示框，这个项目里用什么组件库这里引什么
+import axios from 'axios'
+import { ElMessage } from 'element-plus' // 引入el 提示框，这个项目里用什么组件库这里引什么
 
 // 设置接口超时时间
-axios.defaults.timeout = 60000;
+axios.defaults.timeout = 60000
 
 // 请求地址，这里是动态赋值的的环境变量，下一篇会细讲，这里跳过
 // @ts-ignore
-axios.defaults.baseURL = import.meta.env.VITE_API_DOMAIN;   
+axios.defaults.baseURL = import.meta.env.VITE_API_DOMAIN
 
 //http request 拦截器
 axios.interceptors.request.use(
-  config => {
-  // 配置请求头
+  (config: any) => {
+    // 配置请求头
     config.headers = {
       //'Content-Type':'application/x-www-form-urlencoded',   // 传参方式表单
-      'Content-Type':'application/json;charset=UTF-8',        // 传参方式json
-      'token':'80c483d59ca86ad0393cf8a98416e2a1'              // 这里自定义配置，这里传的是token
-    };
-    return config;
+      'Content-Type': 'application/json;charset=UTF-8', // 传参方式json
+      token: '80c483d59ca86ad0393cf8a98416e2a1' // 这里自定义配置，这里传的是token
+    }
+    return config
   },
-  error => {
-    return Promise.reject(error);
+  (error) => {
+    return Promise.reject(error)
   }
-);
+)
 
 //http response 拦截器
 axios.interceptors.response.use(
-  response => {
-    return response;
+  (response) => {
+    return response
   },
-  error => {
-    const {response} = error;
+  (error) => {
+    const { response } = error
     if (response) {
       // 请求已发出，但是不在2xx的范围
-      showMessage(response.status);           // 传入响应码，匹配响应码对应信息
-      return Promise.reject(response.data);
+      showMessage(response.status) // 传入响应码，匹配响应码对应信息
+      return Promise.reject(response.data)
     } else {
-      ElMessage.warning('网络连接异常,请稍后再试!');
+      ElMessage.warning('网络连接异常,请稍后再试!')
     }
   }
-);
+)
 
 // 封装 GET POST 请求并导出
-export function request(url='',params={},type='POST'){
-//设置 url params type 的默认值
-return new Promise((resolve,reject)=>{
-  let promise
-  if( type.toUpperCase()==='GET' ){
-    promise = axios({
-      url,
-      params
-    })
-  }else if( type.toUpperCase()=== 'POST' ){
-    promise = axios({
-      method:'POST',
-      url,
-      data:params
-    })
-  }
-  //处理返回
-  promise.then(res=>{
-    resolve(res)
-  }).catch(err=>{
-    reject(err)
+export function request(url = '', params = {}, type = 'POST') {
+  //设置 url params type 的默认值
+  return new Promise((resolve, reject) => {
+    let promise
+    if (type.toUpperCase() === 'GET') {
+      promise = axios({
+        url,
+        params
+      })
+    } else if (type.toUpperCase() === 'POST') {
+      promise = axios({
+        method: 'POST',
+        url,
+        data: params
+      })
+    }
+    //处理返回
+    promise
+      ?.then((res) => {
+        resolve(res)
+      })
+      .catch((err) => {
+        reject(err)
+      })
   })
-})
 }
 
-export const showMessage = (status:number|string) : string => {
-  let message:string = "";
+export const showMessage = (status: number | string): string => {
+  let message: string = ''
   switch (status) {
-      case 400:
-          message = "请求错误(400)";
-          break;
-      case 401:
-          message = "未授权，请重新登录(401)";
-          break;
-      case 403:
-          message = "拒绝访问(403)";
-          break;
-      case 404:
-          message = "请求出错(404)";
-          break;
-      case 408:
-          message = "请求超时(408)";
-          break;
-      case 500:
-          message = "服务器错误(500)";
-          break;
-      case 501:
-          message = "服务未实现(501)";
-          break;
-      case 502:
-          message = "网络错误(502)";
-          break;
-      case 503:
-          message = "服务不可用(503)";
-          break;
-      case 504:
-          message = "网络超时(504)";
-          break;
-      case 505:
-          message = "HTTP版本不受支持(505)";
-          break;
-      default:
-          message = `连接出错(${status})!`;
+    case 400:
+      message = '请求错误(400)'
+      break
+    case 401:
+      message = '未授权，请重新登录(401)'
+      break
+    case 403:
+      message = '拒绝访问(403)'
+      break
+    case 404:
+      message = '请求出错(404)'
+      break
+    case 408:
+      message = '请求超时(408)'
+      break
+    case 500:
+      message = '服务器错误(500)'
+      break
+    case 501:
+      message = '服务未实现(501)'
+      break
+    case 502:
+      message = '网络错误(502)'
+      break
+    case 503:
+      message = '服务不可用(503)'
+      break
+    case 504:
+      message = '网络超时(504)'
+      break
+    case 505:
+      message = 'HTTP版本不受支持(505)'
+      break
+    default:
+      message = `连接出错(${status})!`
   }
-  return `${message}，请检查网络或联系管理员！`;
-};
+  return `${message}，请检查网络或联系管理员！`
+}
