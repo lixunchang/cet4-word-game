a7112444eb646675d598728ad67195f5d4d9b76a
gexinpai <783263503@qq.com>
2024-03-21 00:54:01 +0800

    feat: 字母音频放本地

---
+++ src/utils/letter.ts
@@ -0,0 +1,108 @@
+const lettersAudio: any = {
+  a:{
+    url: '/letters/a.mp3',
+    volume: 0.5,
+  },
+  b:{
+    url: '/letters/b.mp3',
+    volume: 0.5,
+  },
+  c:{
+    url: '/letters/c.mp3',
+    volume: 0.4,
+  },
+  d:{
+    url: '/letters/d.mp3',
+    volume: 0.5,
+  },
+  e:{
+    url: '/letters/e.mp3',
+    volume: 0.5,
+  },
+  f:{
+    url: '/letters/f.mp3',
+    volume: 0.5,
+  },
+  g:{
+    url: '/letters/g.mp3',
+    volume: 0.5,
+  },
+  h:{
+    url: '/letters/h.mp3',
+    volume: 0.5,
+  },
+  i:{
+    url: '/letters/i.mp3',
+    volume: 0.5,
+  },
+  j:{
+    url: '/letters/j.mp3',
+    volume: 0.5,
+  },
+  k:{
+    url: '/letters/k.mp3',
+    volume: 0.5,
+  },
+  l:{
+    url: '/letters/l.mp3',
+    volume: 0.5,
+  },
+  m:{
+    url: '/letters/m.mp3',
+    volume: 0.5,
+  },
+  n:{
+    url: '/letters/n.mp3',
+    volume: 0.5,
+  },
+  o:{
+    url: '/letters/o.mp3',
+    volume: 0.5,
+  },
+  p:{
+    url: '/letters/p.mp3',
+    volume: 0.5,
+  },
+  q:{
+    url: '/letters/q.mp3',
+    volume: 0.5,
+  },
+  r:{
+    url: '/letters/r.mp3',
+    volume: 0.5,
+  },
+  s:{
+    url: '/letters/s.mp3',
+    volume: 0.5,
+  },
+  t:{
+    url: '/letters/t.mp3',
+    volume: 0.5,
+  },
+  u:{
+    url: '/letters/u.mp3',
+    volume: 0.5,
+  },
+  v:{
+    url: '/letters/v.mp3',
+    volume: 0.5,
+  },
+  w:{
+    url: '/letters/w.mp3',
+    volume: 0.5,
+  },
+  x:{
+    url: '/letters/x.mp3',
+    volume: 0.5,
+  },
+  y:{
+    url: '/letters/y.mp3',
+    volume: 0.5,
+  },
+  z:{
+    url: '/letters/z.mp3',
+    volume: 0.5,
+  },
+}
+
+export default lettersAudio;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,31 @@
 <script setup lang="ts">
 import { DictionaryService } from '@/api/api';
+import lettersAudio from '@/utils/letter';
 import { onMounted, onUnmounted, reactive, computed, watch } from 'vue';
 import { ElMessage } from 'element-plus';
+import { useRouter } from 'vue-router';
+import { ElLoading } from 'element-plus'
+ 
+const { currentRoute } = useRouter();
+const route = currentRoute.value;
+
 const state: any = reactive({
   total: 0,
   list: [],
   current: 1,
   audioIndex: 0,
+  explainStatus: '',
   explain: {
 
   }
-    // {
-    //   mean_en:'',
-    //   mean_cn:'',
-    //   word_etyma: '',
-    //   sentence: '',
-    //   sentence_trans: '',
-    // }
 })
 
 const currentWord = computed(()=>{
-  return state.list[state.current] || ''
+  return route.query.word || state.list?.[state.current]?.split(' ')[0] || ''
+})
+
+const currentExplain = computed(()=>{
+  return state.explain[currentWord.value]
 })
 
 const handleClickSentence=(sentence: string)=>{
@@ -30,8 +35,11 @@ const handleClickSentence=(sentence: string)=>{
 
 
 const handleKeyPress = (event:any) => {
-  console.log('next', event)
-  let audio;
+  // if(!state.currentWord){
+  //   //state.audioIndex>=0
+  //   return;
+  // }
+  let audio:any;
   if(['ArrowRight','ArrowUp'].includes(event.key)){
     state.current++;
     state.audioIndex = 0
@@ -50,16 +58,24 @@ const handleKeyPress = (event:any) => {
       state.audioIndex = 0
     }
   }else if(currentWord.value[state.audioIndex % (currentWord.value.length)]===event.key){
-    audio =new Audio(`http://dict.youdao.com/dictvoice?type=0&audio=${event.key.toUpperCase()}`);
-    audio.playbackRate = 1.9; // 播放速度为0.5 - 2倍
+    const config = lettersAudio[event.key];
+    audio = new Audio(config.url);
+    audio.playbackRate = 2; // 播放速度为0.5 - 2倍
+    Object.keys(config).forEach(key=>{
+      if(key!=='url'){
+        audio[key] = config[key];
+      }
+    });
     if(state.audioIndex > (currentWord.value.length-1)*2){
       state.audioIndex = 0
     }else{
       state.audioIndex++;
     }
   }else{
-    console.log(state.audioIndex, currentWord.value.length, state.audioIndex % (currentWord.value.length - 1))
-    ElMessage.error('你错了, 正确的字母是：' + currentWord.value[state.audioIndex % (currentWord.value.length - 1)])
+    const errorChar = currentWord.value[state.audioIndex % currentWord.value.length];
+    if(errorChar){
+      ElMessage.error('你错了, 正确的字母是：' + currentWord.value[state.audioIndex % currentWord.value.length])
+    }
   }
   if(audio){
     audio.play();
@@ -69,7 +85,7 @@ const handleKeyPress = (event:any) => {
 onMounted(() => {  
   window.addEventListener('keydown', handleKeyPress);
   const wordsJson = localStorage.getItem('word_list');
-  state.current = localStorage.getItem('current_index')||0;
+  state.current = route.query.index||localStorage.getItem('current_index')||0;
   if(wordsJson){
     const data = JSON.parse(wordsJson);
     state.list = data.list;
@@ -84,6 +100,10 @@ onMounted(() => {
 
 });
 
+watch(state.current,(val:number)=>{
+  localStorage.set('max_index',Math.max(val, localStorage.get('max_index')||0) );
+})
+
 watch(currentWord, (newWord: string)=>{
   console.log('newWord', newWord)
   const wordJson = localStorage.getItem(newWord);
@@ -91,9 +111,19 @@ watch(currentWord, (newWord: string)=>{
     state.explain[newWord] = JSON.parse(wordJson);
     return;
   }
+  state.explainStatus = 'loading';
+  const loadingInstance = ElLoading.service({
+    target: '.explain_status',
+    text: '这个单词你认识吗...'
+  })
   DictionaryService.getWordMean(newWord).then(({data}:any)=>{
     state.explain[newWord] = data;
     localStorage.setItem(newWord, JSON.stringify(data))
+    state.explainStatus = '';
+  }).catch(()=>{
+    state.explainStatus = 'error';
+  }).finally(()=>{
+    loadingInstance.close()
   })
 })
 
@@ -101,12 +131,19 @@ onUnmounted(() => {
   window.removeEventListener('keydown', handleKeyPress);
 });
 
+const handleReload=()=>{
+  location.reload();
+}
+
 </script>
 
 <template>
   <main>
+    <div class="header">
+      <span class="steps">{{state.current}} / {{state.list.length}}</span>
+    </div>
     <div class="playground" :data-word="currentWord">
-      <span class="char" v-for="(char,index) in currentWord" :key="currentWord+'-'+char" :class="{
+      <span class="char" v-for="(char,index) in currentWord" :key="currentWord+'-'+index" :class="{
         'audio_char': state.audioIndex < currentWord.length && state.audioIndex > index,
         'equal_char': state.audioIndex!==0 && state.audioIndex % (currentWord.length) === 0,
         'reserve_char': state.audioIndex >= currentWord.length && state.audioIndex % (currentWord.length+1) < index
@@ -114,31 +151,66 @@ onUnmounted(() => {
         {{char}}
       </span>
     </div>
-    <div class="explain" v-if="state.explain[currentWord]">
-      <p class="chinese"><label>中文：</label>
-        <span style="color:red;font-weight: bold;"><span style="flex:1;">{{state.explain[currentWord].mean_cn}}</span></span>
-      </p>
-      <p><label>词根：</label><span style="flex:1;">{{state.explain[currentWord].word_etyma}}</span></p>
-      <p style="color:#999;cursor:pointer;" @click="handleClickSentence(state.explain[currentWord].mean_en)">
-        <label>英文：</label>
-        <span style="flex:1;" class="ellipsis_word">{{state.explain[currentWord].mean_en}}</span>
-      </p>
-      <p @click="handleClickSentence(state.explain[currentWord].sentence)" style="cursor: pointer;"><label>例句：</label><span style="flex:1;">{{state.explain[currentWord].sentence}}</span></p>
-      <p class="chinese"><label>翻译：</label><span style="flex:1;">{{state.explain[currentWord].sentence_trans}}</span></p>
+    <div class="explain_status">
+      <div class="explain" v-if="currentExplain">
+        <p style="color:#999;cursor:pointer;" @click="handleClickSentence(currentExplain.mean_en)">
+          <label>英文：</label>
+          <span style="flex:1;" class="ellipsis_word">{{currentExplain.mean_en}}</span>
+        </p>
+        <p class="chinese">
+          <label>中文：</label>
+          <span style="flex:1;text-align:left;color:red;font-weight: bold;">
+            <span style="flex:1;">{{currentExplain.mean_cn}}</span>
+          </span>
+        </p>
+        <p v-if="currentExplain.word_etyma">
+          <label>词根：</label>
+          <span style="flex:1;color:#555;">{{currentExplain.word_etyma}}</span>
+        </p>
+        <p @click="handleClickSentence(currentExplain.sentence)" style="cursor: pointer;">
+          <label>例句：</label>
+          <span style="flex:1;color:#666;">{{currentExplain.sentence}}</span>
+        </p>
+        <p>
+          <label>翻译：</label>
+          <span style="flex:1;text-align:left;color:#b2b2b2;">{{currentExplain.sentence_trans}}</span>
+        </p>
+      </div>
+      <div v-else-if="state.explainStatus">
+        <span v-if="state.explainStatus === 'loading'"></span>
+        <span style="cursor:pointer;" v-if="state.explainStatus === 'error'" @click="handleReload">查询失败，请刷新重试</span>
+      </div>
     </div>
   </main>
 </template>
 <style lang="scss" scoped>
+  main{
+    height: 100%;
+    position: relative;
+    .header{
+      width: 100%;
+      padding: 8px 12px;
+      position: absolute;
+      top: 0;
+      left: 0;
+      text-align: right;
+      .steps{
+        color:#222;
+      }
+    }
+  }
   .playground{
     background-color: black;
     height: 60vh;
     display: flex;
     align-items: center;
     justify-content: center;
-    font-size: 80px;
+    font-size: 88px;
     font-weight: bold;
+    letter-spacing: 12px;
     .char{
-      width: 60px;
+      width: auto;
+      min-width: 30px;
       height: 100%;
       color: white;
       vertical-align: middle;
@@ -161,17 +233,20 @@ onUnmounted(() => {
     padding: 24px;
     font-size: 26px;
     color: #333;
+    max-width: 1000px;
+    margin: 0 auto;
     .chinese{
       color: #666;
     }
     label{
       display: inline-block;
       width: 120px;
-      color: #999;
+      color: #e2e2e2;
     }
     p{
       overflow: hidden;
       display: flex;
+      justify-content: space-between;
       -webkit-line-clamp: 2; /* 控制显示的行数 */
       -webkit-box-orient: vertical;
       text-overflow: ellipsis;
@@ -182,7 +257,14 @@ onUnmounted(() => {
         -webkit-box-orient: vertical;
         text-overflow: ellipsis;
         white-space: normal;
+        text-align: left;
       }
     }
   }
-</style>
+  .explain_status{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 40vh;
+  }
+</style>@/constant/letter
