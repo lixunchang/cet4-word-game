69e48a6dce82298403b4754e5a32640b20e03f95
gexinpai <783263503@qq.com>
2024-05-26 02:30:43 +0800

    feat: 更新引用

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import TestView from '../views/test/index.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL), //createWebHistory(import.meta.env.BASE_URL),
@@ -18,16 +17,7 @@ const router = createRouter({
     {
       path: '/test',
       name: 'test',
-      component: TestView
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
